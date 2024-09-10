1cd755f399b4e2e0007a8ddeb07fa81259cf6877
takker99 <37929109+takker99@users.noreply.github.com>
2024-09-10 12:19:15 +0900

    test: Use dynamic imports

--- mod.test.ts
+++ mod.test.ts
@@ -1,4 +1,3 @@
-import * as path from "@std/path";
 import { assert, assertEquals, assertThrows } from "@std/assert";
 import { type ImportMap, isImportMap } from "./import_map.ts";
 import { resolveImportMap, resolveModuleSpecifier } from "./resolve.ts";
@@ -27,35 +26,34 @@ function runTests(
     fn: () => {
       if (!isImportMap(importMap) || expectedParsedImportMap === null) {
         assert(!isImportMap(importMap));
-      } else {
-        const resolvedImportMap = resolveImportMap(
-          importMap,
-          new URL(importMapBaseURL),
-        );
-        if (expectedParsedImportMap) {
-          assertEquals(resolvedImportMap, expectedParsedImportMap);
-        }
-        if (expectedResults) {
-          for (
-            const [key, expectedResult] of Object.entries(expectedResults)
-          ) {
-            if (expectedResult === null) {
-              assertThrows(() => {
-                resolveModuleSpecifier(
-                  key,
-                  resolvedImportMap,
-                  new URL(baseURL),
-                );
-              });
-            } else {
-              const resolvedModuleSpecifier = resolveModuleSpecifier(
-                key,
-                resolvedImportMap,
-                new URL(baseURL),
-              );
-              assertEquals(resolvedModuleSpecifier, expectedResult);
-            }
-          }
+        return;
+      }
+      const resolvedImportMap = resolveImportMap(
+        importMap,
+        new URL(importMapBaseURL),
+      );
+      if (expectedParsedImportMap) {
+        assertEquals(resolvedImportMap, expectedParsedImportMap);
+      }
+      if (!expectedResults) return;
+      for (
+        const [key, expectedResult] of Object.entries(expectedResults)
+      ) {
+        if (expectedResult === null) {
+          assertThrows(() => {
+            resolveModuleSpecifier(
+              key,
+              resolvedImportMap,
+              new URL(baseURL),
+            );
+          });
+        } else {
+          const resolvedModuleSpecifier = resolveModuleSpecifier(
+            key,
+            resolvedImportMap,
+            new URL(baseURL),
+          );
+          assertEquals(resolvedModuleSpecifier, expectedResult);
         }
       }
     },
@@ -69,28 +67,31 @@ function createTests(name: string, data: TestData) {
     importMapBaseURL,
     baseURL,
   } = data;
-  if (tests) {
-    for (const [testName, test] of Object.entries(tests)) {
-      const combinedName = `${name} → ${testName}`;
-      const inheritedTestData = {
-        importMap: test.importMap || importMap,
-        importMapBaseURL: test.importMapBaseURL || importMapBaseURL,
-        baseURL: test.baseURL || baseURL,
-        expectedParsedImportMap: test.expectedParsedImportMap,
-        expectedResults: test.expectedResults,
-        tests: test.tests,
-      };
-      createTests(combinedName, inheritedTestData);
-    }
-  } else {
+  if (!tests) {
     runTests(name, data);
+    return;
+  }
+  for (const [testName, test] of Object.entries(tests)) {
+    const combinedName = `${name} → ${testName}`;
+    const inheritedTestData = {
+      importMap: test.importMap || importMap,
+      importMapBaseURL: test.importMapBaseURL || importMapBaseURL,
+      baseURL: test.baseURL || baseURL,
+      expectedParsedImportMap: test.expectedParsedImportMap,
+      expectedResults: test.expectedResults,
+      tests: test.tests,
+    };
+    createTests(combinedName, inheritedTestData);
   }
 }
 
 // testdata from https://github.com/web-platform-tests/wpt/tree/master/import-maps/data-driven/resources
-const testdataDir = "testdata";
+const testdataDir = "./testdata";
 for await (const { name: fileName } of Deno.readDir(testdataDir)) {
-  const file = await Deno.readTextFile(path.join(testdataDir, fileName));
-  const data = JSON.parse(file);
-  createTests(fileName, data);
+  createTests(
+    fileName,
+    (await import(`./${testdataDir}/${fileName}`, {
+      with: { type: "json" },
+    })).default,
+  );
 }
